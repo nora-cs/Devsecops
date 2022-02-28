a80be5c4990670c50f94cc389a4344a72e1212f3
nora galabe <nchang.najeh@gmail.com>
2022-02-27 21:38:54 -0500

    added new file

---
+++ Question_of_day.py
@@ -0,0 +1,91 @@
+from mimetypes import init
+
+
+Question of day for Tuesday February 15th
+#What is the command to clear your terminal display
+ctr+l, clear
+#What is the command to delete a file
+rm <file>
+#What command is used to change directories
+cd <dir>
+#what command is used to get the ip address of all interfaces on a server
+ip addr show, ifconfig
+#what command is used to change the ownership of a file??
+chown <new-owner> <filename>
+
+Question of day: Wednesday February 16th 2022
+#What is the default text editor for the Bash shell with a Windows-based Git install?
+vim
+#Before you install Git, which of the following prerequisite products must be present and configured on your local OS?
+#a. Apache Maven
+#b. Java Development Kit 1.8 or newer
+#c. Nothing
+#d. Jarkarta Enterprise compliant web server
+C
+
+#After you install Git and prior to issuing the first commit, which two configuration properties does the tool expect to be configured?
+username and email address
+#Who is attributed with the creation of git?
+Linus Torvalds
+#After you initialize a new Git repository and create a file named git-quiz.html, which of the following commands will not work if issued?
+#a. git add git-quiz.html
+#b. git status
+#c. git add .
+#d. git commit -m "git quiz web file added"
+a
+#Which command should you use to initialize a new Git repository?
+git init
+
+Question of day, Monday February 21st 2022
+#what is the output of the following code
+def calculate (num1, num2=4):
+  res = num1 * num2
+  print(res)
+calculate(5, 6)
+30
+#What is the output of the following code?
+var = "James" * 2  * 3
+print(var)
+JamesJamesJamesJamesJamesJames
+
+#what is the output of the following code
+x = 36 / 4 * (3 +  2) * 4 + 2
+print(x)
+182.0
+#What is the output of the following code
+var1 = 1
+var2 = 2
+var3 = "3"
+print(var + var2 + var3)
+can only concatenate str (not "int") to str
+#what is the output of the following code?
+p, q, r = 10, 20 ,30
+print(p, q, r)
+10 20 30
+
+Question of day, February 22nd 2022
+#what is the maximum length of a python idntifier?
+79 characters
+#which of the following is an invalid statement
+num = 1000000
+x y z = 1 2 3
+x,y,z = 1, 2, 3
+x_y_z = 10000000 #this
+#Which of the following are valid Python variable names:
+ver1.3
+return #this
+home_address #this
+route66
+Age #this
+4square
+#All keywords in Python are in _________ *
+Lower case #true 
+Upper case
+None of the mentioned
+Capitalized
+#Which of the following are Python reserved words (keywords):
+default
+and #true
+goto
+class #true
+None #true
